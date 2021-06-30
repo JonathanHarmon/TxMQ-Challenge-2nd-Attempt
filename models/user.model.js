34e5493d3c3683d660755fc24955f72056ab8c53
Jonathan Harmon <jonathan@jonathanharmon.com>
2021-06-30 17:33:42 -0400

    Updates to User Model

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 var userSchema = new mongoose.Schema({
     fullName: {
-        type: String
+        type: String,
+        required: 'Full name can\'t be empty'
     },
     email: {
-        type: String
+        type: String,
+        required: 'Email can\'t be empty'
     },
     password: {
-        type: String
+        type: String,
+        required: 'Password can\'t be empty'
     },
     saltSecret: String
+});
+
+// Events
+
+userSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        })
+    });
 });
 
 mongoose.model('User', userSchema);
