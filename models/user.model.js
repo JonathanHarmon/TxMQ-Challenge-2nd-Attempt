7f005ace6187e0a4861f8db385274e7b1500cfd8
Jonathan Harmon <jonathan@jonathanharmon.com>
2021-07-01 09:43:53 -0400

    Validation to userSchema in user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,12 @@ var userSchema = new mongoose.Schema({
     saltSecret: String
 });
 
+// Custom validation for email
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
 // Events
 
 userSchema.pre('save', function (next) {
